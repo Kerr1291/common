c2ebf2457611ca916e7a62dba9eabe74fda81524
kerr1291 <libra.ae@gmail.com>
2018-08-17 17:19:50 -0700

    Added show UI in text helper. (Currently incomplete)

---
+++ lib/UIHelpers/ShowMemberInUIText.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Reflection;
+using System.Linq;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Text))]
+public class ShowMemberInUIText : MonoBehaviour
+{
+    [SerializeField]
+    Object target;
+
+    [SerializeField]
+    Object rootTarget;
+
+    [SerializeField]
+    SerializableMemberInfo targetRef;
+
+    [SerializeField] 
+    Text uiText; 
+    
+    void Reset()
+    {
+        uiText = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if(target != null)
+        {
+            object targetValue = targetRef.GetValue(target);
+
+            if(targetValue != null)
+                uiText.text = targetValue.ToString();
+            else
+                uiText.text = "null";
+
+            return;
+        }
+
+        uiText.text = "";
+    }
+}
+
+[System.Serializable]
+public class SerializableMemberInfo
+{
+    public MemberInfo Info
+    {
+        get 
+        {
+            //if the data is lost, rebuild it
+            if(info == null)
+            {
+                if(string.IsNullOrEmpty(memberName))
+                    return null;
+
+                if(System.Type.GetType(typeName) == null)
+                    return null;
+
+                if(isProperty)
+                {
+                    info = System.Type.GetType(typeName).GetProperty(memberName, bFlags);
+                }
+                else
+                {
+                    info = System.Type.GetType(typeName).GetField(memberName, bFlags);
+                }
+            }
+
+            return info;
+        }
+        set
+        {
+            info = value;
+            if(info != null)
+            {
+                isProperty = (info as PropertyInfo != null);
+                typeName = info.DeclaringType.FullName;
+                memberName = info.Name;
+                bFlags = info.DeclaringType.GetType().IsNotPublic ? BindingFlags.NonPublic : BindingFlags.Public;
+            }
+        }
+    }
+
+    public object GetValue(object instance)
+    {
+        var fi = Info as FieldInfo; 
+        if(fi != null)
+        {
+            object targetValue = fi.GetValue(instance);
+            return targetValue;
+        }
+        var pi = Info as PropertyInfo;
+        if(pi != null)
+        {
+            object targetValue = pi.GetValue(instance);
+            return targetValue;
+        }
+        return null;
+    }
+
+    public void SetValue(object instance, object value)
+    {
+        var fi = Info as FieldInfo;
+        if(fi != null)
+        {
+            fi.SetValue(instance, value);
+        }
+        var pi = Info as PropertyInfo;
+        if(pi != null)
+        {
+            pi.SetValue(instance, value);
+        }
+    }
+
+    MemberInfo info;
+
+    [SerializeField]
+    bool isProperty;
+
+    [SerializeField]
+    string typeName;
+
+    [SerializeField]
+    string memberName;
+
+    [SerializeField]
+    BindingFlags bFlags;
+}
---
+++ lib/UIHelpers/ShowMemberInUITextEditor.cs
@@ -0,0 +1,86 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+using System.Linq;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(ShowMemberInUIText))]
+public class ShowMemberInUIText_Editor : Editor
+{
+    ShowMemberInUIText Target
+    {
+        get
+        {
+            return ((ShowMemberInUIText)target);
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        return;
+
+        Object currentTarget = (Object)Target.GetType().GetField("target", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(Target);
+        Object rootTarget = (Object)Target.GetType().GetField("rootTarget", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(Target);
+
+        rootTarget = EditorGUILayout.ObjectField("Target Object", rootTarget, typeof(object), true);
+
+        if(rootTarget as GameObject != null)
+        {
+            List<Object> objects = new List<Object>();
+            objects = (rootTarget as GameObject).GetComponents<Component>().Cast<Object>().ToList();
+            objects.Insert(0, rootTarget as GameObject);
+
+            int tarIndex = 0;
+            if(currentTarget != null)
+                tarIndex = objects.Select(x => x != null ? x.GetType().Name : "null").ToList().IndexOf(currentTarget.GetType().Name);
+
+            if(tarIndex < 0)
+                tarIndex = 0;
+
+            tarIndex = EditorGUILayout.Popup("Target Reference", tarIndex, objects.Select(x => x != null ? x.GetType().Name : "null").ToArray());
+
+            currentTarget = objects[tarIndex];
+        }
+        else
+        {
+            currentTarget = rootTarget;
+        }
+
+        Target.GetType().GetField("target", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(Target, currentTarget);
+        Target.GetType().GetField("rootTarget", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(Target, rootTarget);
+
+        if(currentTarget != null)
+        {
+            var publicMembers = currentTarget.GetType().GetMembers().Select(x => x);
+            var nonpublicMembers = currentTarget.GetType().GetMembers(BindingFlags.NonPublic).Select(x => x);
+            //var staticMembers = currentTarget.GetType().GetMembers(BindingFlags.Static).Select(x => x);
+            //var staticNonpublicMembers = currentTarget.GetType().GetMembers(BindingFlags.Static | BindingFlags.NonPublic).Select(x => x);
+
+            var allMembers = publicMembers.Concat(nonpublicMembers).ToList();
+            //allMembers = allMembers.Concat(staticMembers).Concat(staticNonpublicMembers).ToList();
+            var allNonMethods = allMembers.Where(x => (x as MethodInfo) == null).ToList();
+
+            int memIndex = 0;
+            if(currentTarget != null)
+            {
+                SerializableMemberInfo targetRef = (SerializableMemberInfo)Target.GetType().GetField("targetRef", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(Target);
+
+                memIndex = allNonMethods.IndexOf(targetRef.Info);
+
+                if(memIndex < 0)
+                    memIndex = 0;
+
+                var memArrayNames = allNonMethods.Select(x => x.Name).ToArray();
+                var memArray = allNonMethods.ToArray();
+
+                memIndex = EditorGUILayout.Popup("Member", memIndex, memArrayNames);
+
+                if(memIndex < memArray.Length)
+                    targetRef.Info = memArray[memIndex];
+            }
+        }
+    }
+}
+#endif
